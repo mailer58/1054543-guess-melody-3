a19df536b9c80a8485d4fd45f5590b3ccf3204fe
mailer58 <50555861+mailer58@users.noreply.github.com>
2020-03-16 22:11:22 +0300

    Add task 13

--- components/app/app.jsx
+++ components/app/app.jsx
@@ -6,7 +6,7 @@ import ArtistQuestionScreen from "../artist-question-screen/artist-question-scre
 import GameScreen from "../game-screen/game-screen.jsx";
 import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
 import {GameType} from "../../const.js";
-import {withAudioPlayer} from "../../hocs/with-audio-player/with-audio-player.jsx";
+import withAudioPlayer from "../../hocs/with-audio-player/with-audio-player.js";
 
 const GenreQuestionScreenWrapped = withAudioPlayer(GenreQuestionScreen);
 const ArtistQuestionScreenWrapped = withAudioPlayer(ArtistQuestionScreen);
